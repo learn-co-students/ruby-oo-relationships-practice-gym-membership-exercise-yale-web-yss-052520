f905e6844b2dae46b13f29926eb07d567c113e47
Anmolpreet <anmolpreet.kaur@yale.edu>
2020-06-02 16:30:15 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,23 @@ require 'pry'
 require_relative '../lib/gym'
 require_relative '../lib/lifter'
 require_relative '../lib/membership'
+
+lifter1 = Lifter.new("Anmol", 5)
+lifter2 = Lifter.new("Mushi", 10)
+lifter3 = Lifter.new("Mom", 20)
+lifter4 = Lifter.new("Dad", 4)
+
+gym1 = Gym.new("Gym1")
+gym2 = Gym.new("Gym2")
+
+m1 = Membership.new(5, lifter1, gym1)
+m2 = Membership.new(6, lifter2, gym1)
+m3 = Membership.new(7, lifter3, gym2)
+m4 = Membership.new(8, lifter4, gym2)
+m5 = Membership.new(9, lifter1, gym2)
+
+binding.pry 
+0
+
+
+
--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,35 @@
 class Lifter
   attr_reader :name, :lift_total
+  @@all = []
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+  def self.all 
+    @@all 
+  end 
+
+  def memberships 
+    Membership.all.select{|membership| membership.lifter == self}
+  end 
+
+  def gyms 
+    memberships.map{|membership| membership.gym}
+  end 
+
+  def self.average_lift_total
+    sum = all.collect{|lifter| lifter.lift_total}.sum
+    sum / all.size.to_f
+  end 
+
+  def total_cost
+    memberships.collect{|membership| membership.cost}.sum
+  end 
+
+  def sign_up(gym, cost)
+    Membership.new(cost, self, gym)
+  end 
 end
