831f32a360b700a606d83e1a00b4491b6f08737a
Dennis <Dennis.portillo@yale.edu>
2020-06-02 12:04:47 -0700

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,38 @@
 class Lifter
   attr_reader :name, :lift_total
 
+  @@all = []
+
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select {|membership| membership.lifter == self}
+  end
+
+  def gyms
+     memberships.map {|membership| membership.gym}
+  end 
+
+  def self.average_lift_total
+    total_lift_amount = self.all.reduce(0){|sum, lifter| sum +lifter.lift_total}
+    number_of_lifters = self.all.count
+    average= (total_lift_amount)/(number_of_lifters)
+  end
+
+  def gym_cost
+    memberships.reduce(0){|sum, membership| sum + membership.cost}
+  end
+
+  def sign_lifter(gym, cost)
+    Membership.new(cost, self, gym)
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,23 @@
 require_relative '../config/environment.rb'
 
 # test code goes here
+l1 = Lifter.new("Dennis", 100)
+l2 = Lifter.new("Alex", 150)
+l3 = Lifter.new("Brian", 10)
+l4 = Lifter.new("Noah", 70)
+l5 = Lifter.new("Joe", 120)
+
+g1 = Gym.new("GAINZ")
+g2 = Gym.new("WHEY")
+g3 = Gym.new("PROTEIN")
+g4 = Gym.new("DUMBBELL")
+g5 = Gym.new("BENCHPRESS")
+
+m1 = Membership.new(25, l1, g1)
+m2 = Membership.new(75, l3, g3)
+m3 = Membership.new(50, l1, g2)
+m4 = Membership.new(25, l4, g1)
+m5 = Membership.new(100, l5, g4)
 
 binding.pry
 
