93f8cc5e53a735afef863435cbff4610b19d95ad
dlee3427 <david.lee.sl2634@yale.edu>
2020-06-02 14:48:42 -0400

    done

--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,20 @@
 require_relative '../config/environment.rb'
 
 # test code goes here
+l1 = Lifter.new("John Cena", 0)
+l2 = Lifter.new("Michael Jordan", 200)
+l3 = Lifter.new("Lebron James", 100)
+
+g1 = Gym.new("Fitness Gym")
+g2 = Gym.new("Workout Gym")
+g3 = Gym.new("Best Gym")
+
+m1 = Membership.new(g1, l1, 50)
+m2 = Membership.new(g2, l1, 500)
+m3 = Membership.new(g1, l2, 20)
+m4 = Membership.new(g3, l3, 100)
+
+
 
 binding.pry
 
