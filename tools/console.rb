76e01f2ffe4ceb9a54532a81cddab75ddf05aa86
Jonkavaliauskas <jonas.kavaliauskas@yale.edu>
2020-06-03 01:19:01 +0300

    Done

--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,18 @@ require_relative '../config/environment.rb'
 
 # test code goes here
 
+pokegym = Gym.new("pokegym")
+impuls = Gym.new("impuls")
+
+tom = Lifter.new("Tom", 50)
+eric = Lifter.new("Eric", 100)
+
+pokegym_tom = Membership.new(100, pokegym, tom)
+impuls_eric = Membership.new(200, impuls, eric)
+impuls_tom = Membership.new(200, impuls, tom)
+
+
+
 binding.pry
 
 puts "Gains!"
