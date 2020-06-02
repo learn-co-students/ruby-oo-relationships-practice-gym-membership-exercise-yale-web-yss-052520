24011d48fdb2bc7c07843a7e032a82387c2aa0df
chad <chad.palmer@yale.edu>
2020-06-02 13:59:58 -0400

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,31 @@
 class Gym
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select {|membership| membership.gym == self}
+  end
+
+  def lifters
+    memberships.map {|membership| membership.lifter}.uniq
+  end
+
+  def names_of_lifters
+    lifters.map {|lifter| lifter.name}.uniq
+  end
+
+  def combined_lift_total
+    lifters.sum {|lifter| lifter.lift_total}
   end
 end
