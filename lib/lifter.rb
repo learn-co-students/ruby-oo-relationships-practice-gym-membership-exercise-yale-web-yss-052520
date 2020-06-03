c90e30475b64012ac96a3ab71c0429f67a083050
arthur318 <arthurhwang318@gmail.com>
2020-06-02 23:30:49 -0700

    done

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,35 @@
 class Lifter
   attr_reader :name, :lift_total
+  @@all = []
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def get_memberships
+    Membership.all.select {|membership| membership.lifter == self}
+  end
+
+  def get_gyms
+    get_memberships.map {|membership| membership.gym}
+  end
+
+  def self.avg_lift
+    all.sum {|lifter| lifter.lift_total} / all.size
+  end
+
+  def get_cost
+    get_memberships.sum {|lifter| lifter.cost}
+  end
+
+  def self.sign_up(gym, cost)
+    Membership.new(cost, self, gym)
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,18 @@
 require_relative '../config/environment.rb'
 
 # test code goes here
+gym1 = Gym.new("Fitness 19")
+gym2 = Gym.new("Club Sports")
+gym3 = Gym.new("24 Hour")
+
+lifter1 = Lifter.new("Arthur", 200)
+lifter2 = Lifter.new("Bryce", 180)
+lifter3 = Lifter.new("JY", 150)
+
+m1 = Membership.new(20, lifter1, gym1)
+m2 = Membership.new(100, lifter1, gym2)
+m3 = Membership.new(20, lifter2, gym1)
+m4 = Membership.new(20, lifter3, gym1)
 
 binding.pry
 
